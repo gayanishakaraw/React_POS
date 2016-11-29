6fc3bc2aa51ef970c42ebfea957f341bef9bb8c4
kjharesh <kjharesh@gmail.com>
2016-11-29 11:57:05 +0530

    Sign In
    Sing In page

--- source/src/components/SignInNumPad.js
+++ source/src/components/SignInNumPad.js
@@ -15,33 +15,47 @@ constructor(props) {
     this.setState({
       showComponent: true,
     });
+    this.SignInNumPad.div.class = ".Hidden" ;
   }
 
   OnClickDisplayNumber(item){
      //document.getElementById("EmployeeId").value += item;
   }
 
+  HideElements(){
+    document.getElementById("signInPassword").hidden = true;
+    
+  }
+
   render() {
     return (
-      <div class='SignInNumPad'>
-         <h1> Sign In </h1>
-        <textarea id="EmployeeId"></textarea><br/>
-        <button class="input-button" data-char='1' onClick={this.OnClickDisplayNumber}>1</button>
-        <button class="input-button" data-char='2' onClick={this.OnClickDisplayNumber}>2</button>
-        <button class="input-button" data-char='3'>3</button><br/>
-        <button class="input-button" data-char='4'>4</button>
-        <button class="input-button" data-char='5'>5</button>
-        <button class="input-button" data-char='6'>6</button><br/>
-        <button class="input-button" data-char='7'>7</button>
-        <button class="input-button" data-char='8'>8</button>
-        <button class="input-button" data-char='9'>9</button><br/>
-         <button id='SignInNumPad-Sign' onClick={this.OnClickSignIn.bind(this)}>Sign In</button>
-         {this.state.showComponent ?
-           <StartPage /> :
-           null
-        }
-        <button class="input-button" data-char='0'>0</button>
+      <div id="SignInPage" >
+        <div id="NumPadDiv" class='SignInNumPad'>
+        <h1 id="Heading"> Sign In </h1>
+        <textarea id="signInPassword" ></textarea><br/>
+        <button class="NumPad-button" data-char='1'  onClick={this.OnClickDisplayNumber}>1</button>
+        <button class="NumPad-button" data-char='2' onClick={this.OnClickDisplayNumber}>2</button>
+        <button class="NumPad-button" data-char='3'onClick={this.OnClickDisplayNumber}>3</button><br/>
+        <button class="NumPad-button" data-char='4'>4</button>
+        <button class="NumPad-button" data-char='5'>5</button>
+        <button class="NumPad-button" data-char='6'>6</button><br/>
+        <button class="NumPad-button" data-char='7'>7</button>
+        <button class="NumPad-button" data-char='8'>8</button>
+        <button class="NumPad-button" data-char='9'>9</button><br/>
+        <button id='SignInNumPad-Sign' onClick={this.OnClickSignIn.bind(this)}>Sign In</button>
+        <button class="NumPad-button" data-char='0'>0</button>
         <button id='SignInNumPad-Sign' onClick="">Clear</button>
+        </div>
+         {
+           this.state.showComponent ?
+           <StartPage /> :
+            null
+         }
+         {
+           this.state.showComponent ?
+           document.getElementById("NumPadDiv").hidden = true:
+           null
+         }
       </div>
     );
   }
